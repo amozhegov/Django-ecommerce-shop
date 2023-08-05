d0be5e97f1540da63dfd2b83080241de8b56b8c9
amozhegov <kaneImfb@gmail.com>
2023-08-05 20:26:06 +0200

    Added ShippingAddress model

--- app/ecommerce_shop/models.py
+++ app/ecommerce_shop/models.py
@@ -34,3 +34,18 @@ class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.product.name
+
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    address = models.CharField(max_length=300, null=False)
+    city = models.CharField(max_length=100, null=False)
+    state = models.CharField(max_length=100, null=False)
+    zipcode = models.CharField(max_length=100, null=False)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.address
