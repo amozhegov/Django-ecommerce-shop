8b303285abcd4a089f183741d5ed47cee6790e17
amozhegov <kaneImfb@gmail.com>
2023-08-08 19:31:18 +0200

    Added updateUserOrder function

--- app/static/js/cart.js
+++ app/static/js/cart.js
@@ -9,11 +9,32 @@ for (i = 0; i < updateBtns.length; i++) {
         console.log('productId:', productId, ';', 'Action:', action)
         // Make sure user is authenticated
         console.log('User:', user)
+
         if (user === 'AnonymouesUser') {
             console.log('User is not authenticated')
         } else {
-            console.log('User is authenticated.')
+            updateUserOrder(productId, action)
         }
         
+    })
+}
+
+function updateUserOrder(productId, action) {
+    console.log('User is authenticated')
+
+    var url = '/update_item/'
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify({'productId': productId, 'action': action})
+    })
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        console.log('data: ', data)
     })
 }
