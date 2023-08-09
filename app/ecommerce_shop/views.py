1a596dd1d0526ba5c9b4f1b3188d62d2ad3d0ba1
amozhegov <kaneImfb@gmail.com>
2023-08-09 18:31:36 +0200

    Added item addition subtraction

--- app/ecommerce_shop/views.py
+++ app/ecommerce_shop/views.py
@@ -40,5 +40,21 @@ def updateItem(request):
     print('Action:', action)
     print('productId:', productId)
     
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer = customer, complete = False)
+
+    orderItem, created = OrderItem.objects.get_or_create(order = order, product = product)
+
+    if action == 'add':
+        orderItem.quantity += 1
+    elif action == 'remove':
+        orderItem.quantity -= 1
+
+    orderItem.save()
+
+    if OrderItem.quantity <= 0:
+        OrderItem.delete()
+
     return JsonResponse('Item has been added', safe=False)
 
