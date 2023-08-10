506ca345365a1613660609e2053fd4621060f6b7
amozhegov <kaneImfb@gmail.com>
2023-08-10 20:18:23 +0200

    Finished Order Processing

--- app/ecommerce_shop/views.py
+++ app/ecommerce_shop/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 import json
+import datetime
 from .models import *
 
 def store(request):
@@ -75,3 +76,30 @@ def updateItem(request):
 
     return JsonResponse('Item has been added', safe=False)
 
+def processOrder(request):
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+
+    if request.user.is_authenticated:
+        customer= request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        total = float(data['form']['total'])
+        order.transaction_id = transaction_id
+
+        if total == order.get_cart_total:
+            order.complete = True
+        order.save()
+
+        if order.shipping == True:
+            ShippingAddress.objects.create(
+                customer = customer,
+                order = order,
+                address = data['shipping']['address'],
+                city = data['shipping']['city']
+                state = data['shipping']['state'],
+                zipcode = data['shipping']['zipcode'],
+            )
+
+    else:
+        print('User is not Authenticated')
+    return JsonResponse('Payment has been completed', safe=False)
